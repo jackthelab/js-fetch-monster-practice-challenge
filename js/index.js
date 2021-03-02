7bf24f4f7d1675ec7bfa31996b075c72397841db
Jacob Hampton <jhampton792@gmail.com>
2021-03-02 08:00:09 -0600

    connected .json file

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'http://localhost:3000/monsters/'
+
+// optional params
+// _limit=[number]
+// _page=[number]
+// example GET http://localhost:3000/monsters/?_limit=20&_page=3
+// --> returns items 41-60 because each page is 20 items long and this is the 3rd page
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("working")
+})
