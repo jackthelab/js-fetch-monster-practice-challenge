d6d7c1649e84c37660bf8efb9d6378f7f8a2473b
Jacob Hampton <jhampton792@gmail.com>
2021-03-02 11:37:38 -0600

    Added new monster form

--- js/index.js
+++ js/index.js
@@ -7,5 +7,13 @@ const BASE_URL = 'http://localhost:3000/monsters/'
 // --> returns items 41-60 because each page is 20 items long and this is the 3rd page
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("working")
+    // let pageNumber = 2
+    fetchMonsters()
 })
+
+async function fetchMonsters(page=1){
+    const response = await fetch(BASE_URL+`?_limit=50&_page=${page}`)
+    const monsterData = await response.json()
+
+    console.log(monsterData);
+}
